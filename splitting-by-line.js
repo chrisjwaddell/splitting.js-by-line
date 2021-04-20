f4577a510bae4d0e5aa35c0320718489e9da7bd7
Chris Waddell <yesitschris@hotmail.com>
2021-04-20 22:55:14 +1000

    First load

---
+++ splitting-by-line.js
@@ -0,0 +1,26 @@
+const elAllSplitting = document.querySelectorAll('[data-splitting]')
+let elWords
+let elChars
+
+let line = 0
+let linechar = 0
+
+elAllSplitting.forEach((txt) => {
+  line = 0
+  linechar = 0
+  elWords = txt.querySelectorAll('.word.words')
+  for (let w = 0; w < elWords.length; w++) {
+    if (line !== Number(elWords[w].style.getPropertyValue("--line-index"))) {
+      line++
+      linechar = 0
+    }
+    // console.log("line - " + line + ", w - " + w + ", propertyvalue - " + elWords[w].style.getPropertyValue("--line-index"))
+
+    elChars = elWords[w].querySelectorAll('.char')
+    for (let c = 0; c < elChars.length; c++) {
+      elChars[c].style.setProperty("--linechar-index", linechar)
+      elChars[c].style.setProperty("--line-index", line)
+      linechar++
+    }
+  }
+})
