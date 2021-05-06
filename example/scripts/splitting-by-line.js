37e3bf8ab81be4d83302b59ca224bd6835bb458f
Chris Waddell <yesitschris@hotmail.com>
2021-05-06 12:47:09 +1000

    Update example

--- example/scripts/splitting-by-line.js
+++ example/scripts/splitting-by-line.js
@@ -6,30 +6,54 @@ let elChars
 
 let line = 0
 let linechar = 0
-
+let linechartotal = 0
+var linecharmax = 0
 
 elAllSplitting.forEach((txt) => {
-  // debugger
   line = 0
   linechar = 0
   elWords = txt.querySelectorAll('.word.words')
-  console.log(elWords)
-  // debugger
+
   for (let w = 0; w < elWords.length; w++) {
     if (line !== Number(elWords[w].style.getPropertyValue("--line-index"))) {
       line++
       linechar = 0
     }
-    // console.log("line - " + line + ", w - " + w + ", propertyvalue - " + elWords[w].style.getPropertyValue("--line-index"))
+    if (!linechar) {
+	linechartotal = document.querySelectorAll(`.word.words[style*="--line-index:${line}"] .char`).length
+    }
 
     elChars = elWords[w].querySelectorAll('.char')
     for (let c = 0; c < elChars.length; c++) {
       elChars[c].style.setProperty("--linechar-index", linechar)
       elChars[c].style.setProperty("--line-index", line)
+      elChars[c].style.setProperty("--linechar-total", linechartotal)
       elChars[c].classList.add("sbl")
       linechar++
+
+      if (linechartotal > linecharmax) {
+        linecharmax = linechartotal
+      }
+
     }
   }
+
+  txt.style.setProperty("--linechar-max", linecharmax)
 })
 
+let t0 = performance.now();
+
+elAllSplitting.forEach((txt) => {
+    txt.classList.add("sbl1");
+})
+
+let t1 = performance.now();
+
+setTimeout(() => {
+    elAllSplitting.forEach((txt) => {
+        txt.classList.add("sbl2");
+    })
+}, 500)
+
 }
+
