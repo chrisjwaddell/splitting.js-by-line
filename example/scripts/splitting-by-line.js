3eb1c6a2f7c7db52322585bc1ebeb939017b4b4f
Chris Waddell <yesitschris@hotmail.com>
2023-02-06 00:45:28 +1100

    --linechar-max bug fixed, Opacity formula fixed

--- example/scripts/splitting-by-line.js
+++ example/scripts/splitting-by-line.js
@@ -1,55 +1,58 @@
 function splittingByLine() {
 
-const elAllSplitting = document.querySelectorAll('[data-splitting]')
-let elWords
-let elChars
+    const elAllSplitting = document.querySelectorAll('[data-splitting]')
+    let elWords
+    let elChars
 
-let line = 0
-let linechar = 0
-let linechartotal = 0
-var linecharmax = 0
+    let line = 0
+    let linechar = 0
+    let linechartotal = 0
+    var linecharmax = 0
 
-elAllSplitting.forEach((txt) => {
-  line = 0
-  linechar = 0
-  elWords = txt.querySelectorAll('.word.words')
+    elAllSplitting.forEach((txt, txtLineNumber) => {
+        line = 0
+        linechar = 0
+        elWords = txt.querySelectorAll('.word.words')
 
-  for (let w = 0; w < elWords.length; w++) {
-    if (line !== Number(elWords[w].style.getPropertyValue("--line-index"))) {
-      line++
-      linechar = 0
-    }
-    if (!linechar) {
-	linechartotal = document.querySelectorAll(`.word.words[style*="--line-index:${line}"] .char`).length
-    }
 
-    elChars = elWords[w].querySelectorAll('.char')
-    for (let c = 0; c < elChars.length; c++) {
-      elChars[c].style.setProperty("--linechar-index", linechar)
-      elChars[c].style.setProperty("--line-index", line)
-      elChars[c].style.setProperty("--linechar-total", linechartotal)
-      elChars[c].classList.add("sbl")
-      linechar++
 
-      if (linechartotal > linecharmax) {
-        linecharmax = linechartotal
-      }
+        for (let w = 0; w < elWords.length; w++) {
+            if (line !== Number(elWords[w].style.getPropertyValue("--line-index"))) {
+                line++
+                linechar = 0
+            }
+            if (!linechar) {
+                linechartotal = document.querySelectorAll(`.word.words[style*="--line-index:${line}"] .char`).length
+            }
 
-    }
-  }
+            elChars = elWords[w].querySelectorAll('.char')
+            for (let c = 0; c < elChars.length; c++) {
+                elChars[c].style.setProperty("--linechar-index", linechar)
+                elChars[c].style.setProperty("--line-index", line)
+                elChars[c].style.setProperty("--linechar-total", linechartotal)
+                elChars[c].classList.add("sbl")
+                linechar++
 
-  txt.style.setProperty("--linechar-max", linecharmax)
-})
+                if (linechar > linecharmax) {
+                    linecharmax = linechar
+                }
 
-elAllSplitting.forEach((txt) => {
-    txt.classList.add("sbl1");
-})
+            }
 
-setTimeout(() => {
-    elAllSplitting.forEach((txt) => {
-        txt.classList.add("sbl2");
+        }
+
+        txt.style.setProperty("--linechar-max", linecharmax)
     })
-}, 500)
+
+
+    elAllSplitting.forEach((txt) => {
+        txt.classList.add("sbl1");
+    })
+
+    setTimeout(() => {
+        elAllSplitting.forEach((txt) => {
+            txt.classList.add("sbl2");
+        })
+    }, 200)
 
 }
-
